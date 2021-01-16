5768fd47d85e9112b30e0891bee97efc93e570f8
kraxarn <me@kraxarn.com>
2021-01-16 18:21:57 +0100

    v1.1

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 const ApplicationName string = "ytdl-client"
-const CurrentVersion string = "v1.0"
+const CurrentVersion string = "v1.1"
 
 func ApplicationTitle() string {
 	return fmt.Sprintf("ytdl-client %s", CurrentVersion)
